3c3c171f19bf297e8d1c65eddb1d012e4ec7d244
jiahnkim230502 <locker9417@gmail.com>
2023-07-13 14:58:57 +0900

    게시글 작성,수정,제거,목록 및 상세조회

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require("express");
-const cookieParser = require("cookie-parser");
-const userRouter = require("./routes/users.js");
-const postRouter = require("./routes/posts.js");
-const commentRouter = require("./routes/comment.js");
-const likeRouter = require("./routes/likes.js")
 const app = express();
 const PORT = 3018;
 
+const routes = require("./routes");
+const cookieParser = require("cookie-parser");
+
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', [userRouter, postRouter, commentRouter, likeRouter]);
+app.use('/api', routes);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트 번호로 서버가 실행되었습니다.');
---
+++ repositories/posts.repository.js
@@ -0,0 +1,44 @@
+class PostRepository {
+  constructor(postsModel) {
+    this.postsModel = postsModel;
+  }
+  findAllPost = async () => {
+    const posts = await this.postsModel.findAll();
+
+    return posts;
+  };
+
+  findPostById = async (postId) => {
+    const post = await this.postsModel.findByPk(postId);
+
+    return post;
+  };
+
+  createPost = async (nickname, password, title, content) => {
+    const createPostData = await this.postsModel.create({
+      nickname,
+      password,
+      title,
+      content,
+    });
+
+    return createPostData;
+  };
+
+  updatePost = async (postId, password, title, content) => {
+    const updatePostData = await this.postsModel.update(
+      { title, content },
+      { where: { postId, password } }
+    );
+
+    return updatePostData;
+  };
+
+  deletePost = async (postId, password) => {
+    const updatePostData = await this.postsModel.destroy({ where: { postId, password}});
+
+    return updatePostData;
+  };
+};
+
+module.exports = PostRepository;
